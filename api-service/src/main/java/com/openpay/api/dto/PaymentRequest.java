22ffdc51538191ee27b76980165eca98c67e48b9
gracemann365 <gracemann365@gmail.com>
2025-05-21 23:27:16 +0530

    Update : api pre-service layer

--- api-service/src/main/java/com/openpay/api/dto/PaymentRequest.java
+++ api-service/src/main/java/com/openpay/api/dto/PaymentRequest.java
@@ -7,18 +7,18 @@ import com.openpay.api.validation.ValidUpi;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 
-
 public class PaymentRequest {
 
     @ValidUpi
-    @NotBlank(message = "Sender UPI is required")
+    @NotBlank(message = "Sender UPI is required and must be a valid UPI ID")
     private String senderUpi;
 
     @ValidUpi
-    @NotBlank(message = "Receiver UPI is required")
+    @NotBlank(message = "Receiver UPI is required and must be a valid UPI ID")
     private String receiverUpi;
 
     @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    @NotBlank(message = "Amount is required and must be greater than 0")
     private BigDecimal amount;
 
     // Getters & Setters
